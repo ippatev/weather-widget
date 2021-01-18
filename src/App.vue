8728b7654fe2b83b9278b79e1525638ad3b6b597
Alexander Ippatiev <33111889+alexanderippatiev@users.noreply.github.com>
2021-01-18 09:33:33 +0900

    default-location

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="widget">
+    <vnt-dialog
+      :opened.sync="dialogError"
+      title="Warning"
+      content="Near the top, you'll find Access Your Location, and you can uncheck the Use default box and select the permission you prefer, in this case, Allow."
+    >
+      <vnt-dialog-btn-dismiss result="cancel">Ok</vnt-dialog-btn-dismiss>
+    </vnt-dialog>
     <widget-toolbar
       @iconClickHandler="setWindow"
       :icon="getWidgetIcon"
@@ -9,8 +16,8 @@
       <div v-if="showSettingsWindow">
         <settings></settings>
       </div>
-      <div v-else-if="weathers.length === 0" @click="setWindow('')">
-        <vnt-button class="weather-none">Добавить город</vnt-button>
+      <div v-else-if="weathers.length === 0" @click="addMyCity()">
+        <vnt-button class="weather-none">Add my city ☁️</vnt-button>
       </div>
       <div v-else>
         <weather v-for="w in weathers" :key="w.id" :weatherData="w"></weather>
@@ -23,12 +30,13 @@
 import Weather from "@/components/Weather";
 import WidgetToolbar from "@/components/WidgetToolbar";
 import Settings from "@/components/Settings";
-import { mapActions, mapState } from "vuex";
+import { mapMutations, mapActions, mapState } from "vuex";
 export default {
   components: { Weather, WidgetToolbar, Settings },
   name: "App",
   data() {
     return {
+      dialogError: false,
       showSettingsWindow: false,
     };
   },
@@ -43,6 +51,11 @@ export default {
       return this.showSettingsWindow ? "Settings" : "";
     },
   },
+  mounted() {
+    if (this.weathers.length === 0) {
+      // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+    }
+  },
   created() {
     this.loadWeathers();
   },
@@ -50,6 +63,46 @@ export default {
     ...mapActions({
       loadWeathers: "load",
     }),
+    ...mapMutations({
+      addWeather: "ADD_WEATHER",
+    }),
+    async addWeatherHanlder({ lat, lon }) {
+      try {
+        await this.$http
+          .get(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5fefa6da73425bc10f941aab7619eef9&units=metric`
+          )
+          .then((r) => {
+            this.addWeather(r.data);
+          });
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async addMyCity() {
+      if (window.navigator.geolocation) {
+        const ok = await window.navigator.permissions
+          .query({ name: "geolocation" })
+          .then((result) => result.state);
+
+        if (ok === "prompt" || ok === "granted") {
+          window.navigator.geolocation.getCurrentPosition(
+            (success, failure) => {
+              if (failure) {
+                console.error("error ", failure);
+              }
+              console.log(success);
+              this.addWeatherHanlder({
+                lat: success.coords.latitude,
+                lon: success.coords.longitude,
+              });
+            }
+          );
+        } else {
+          this.dialogError = true;
+        }
+      }
+    },
     setWindow(toolbarTitle) {
       switch (toolbarTitle) {
         case "Settings":
